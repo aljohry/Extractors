62d3f6bed595504591f30db1829abb0a05897d6d
Abdalezez aljohry <aljohryabd@gmail.com>
2026-01-04 19:41:59 +0300

    Update scanner.py

--- src/core/scanner.py
+++ src/core/scanner.py
@@ -4,11 +4,8 @@ from src.utils.colors import print_success, print_error, print_step, print_info
 def run_nmap_scan(ip, port):
     nm = nmap.PortScanner()
     
-    # تحديد نوع الفحص بناءً على المدخل
     if port.lower() == 'all':
         print_step(f"Starting FULL PORT SCAN (0-65535) on {ip} ... This may take time!")
-        # -p- يعني فحص كل البورتات
-        # -T4 لزيادة السرعة
         scan_args = '-p- -sS -sV -T4 --version-intensity 3'
         target_port_str = "All Ports"
     else:
@@ -17,18 +14,14 @@ def run_nmap_scan(ip, port):
         target_port_str = str(port)
 
     try:
-        # تنفيذ الفحص
         nm.scan(ip, arguments=scan_args)
         
-        # التأكد من عمل السيرفر
         if ip not in nm.all_hosts():
             raise Exception("Host down or blocking Ping")
 
-        # معالجة النتائج
         found_open_ports = []
         
         if port.lower() == 'all':
-            # جلب كل البورتات المفتوحة
             if 'tcp' in nm[ip]:
                 for p in nm[ip]['tcp']:
                     state = nm[ip]['tcp'][p]['state']
@@ -38,18 +31,16 @@ def run_nmap_scan(ip, port):
                         print_success(f"Found Open Port: {p}/tcp | Service: {svc} {ver}")
                         found_open_ports.append({'port': p, 'service': svc, 'version': ver})
             
-            # نرجع أول بورت مفتوح وجدناه لنكمل عليه باقي العملية (للتبسيط)
             if found_open_ports:
                 return {'service': found_open_ports[0]['service'], 
                         'version': found_open_ports[0]['version'], 
                         'state': 'open',
-                        'port': found_open_ports[0]['port']} # نحفظ البورت الذي وجدناه
+                        'port': found_open_ports[0]['port']} 
             else:
                 print_error("No open ports found.")
                 return None
 
         else:
-            # الفحص العادي لبورت واحد
             p = int(port)
             if p in nm[ip]['tcp']:
                 state = nm[ip]['tcp'][p]['state']
